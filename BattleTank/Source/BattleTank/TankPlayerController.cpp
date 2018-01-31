6f27c1fe7bc25a4ce31e3b6c61a5029ec41a6643
Jared-PC\Jared <jhparris@gmail.com>
2018-01-30 18:31:15 -0600

    Added tick method to Player Controller

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -17,7 +17,26 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	// get world location if linetrace through crosshair
+	// if it hits the landscape
+		// tell controlled tank to aim at this point
 }
