0e27cc7617ac2b912a540e0acbefb68a641c2833
Jared-PC\Jared <jhparris@gmail.com>
2018-01-30 19:15:09 -0600

    Created shell of method to get the location of the crosshair

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -36,7 +36,24 @@ void ATankPlayerController::AimTowardsCrosshair()
 		return;
 	}
 
-	// get world location if linetrace through crosshair
-	// if it hits the landscape
-		// tell controlled tank to aim at this point
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		// TODO tell controlled tank to aim at this point
+	}
+}
+
+// Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	// ray cast from crosshair to world
+	// if ray cast target is part of the world
+		// grab location, store into out parameter
+		// return hit location
+	// if world not hit, return false
+
+	OutHitLocation = FVector(1.0);
+
+	return true;
 }
