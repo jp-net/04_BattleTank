3a65e82399f8f259f9b7e22856c19b2ebc56f2fb
Jared-PC\Jared <jhparris@gmail.com>
2018-02-03 14:33:09 -0600

    Refactored some code from the previous commit and started working on a new method to move the tank's barrel

--- BattleTank/Source/BattleTank/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/TankAimingComponent.h
@@ -18,16 +18,12 @@ public:
 
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
+	// TODO add SetTurretReference
+
 	void AimAt(FVector HitLocation, float LaunchSpeed);
-
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
 
 private:
 	UStaticMeshComponent* Barrel = nullptr;
-
+	
+	void MoveBarrelTowards(FVector AimDirection);
 };
